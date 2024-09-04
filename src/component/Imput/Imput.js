09014901bdbed4d1d320a4fa705656f006b834fa
Allan Cauê R. Moura <allancaue@gmail.com>
2024-09-03 23:36:28 -0300

    feat: :hammer: Creating a password reset and view page
    Creation of the password reset page and addition of the functionality to view the password

--- src/component/Imput/Imput.js
+++ src/component/Imput/Imput.js
@@ -1,17 +1,38 @@
+import { useState } from 'react';
 import style from './Imput.module.css';
 
-function Imput({ type, placeholder, value, onChange, imagen1, imagen2 }) {
+function Imput({ type, placeholder, value, onChange, imagen1, imagen2, imagen3 }) {
+    const [inputType, setInputType] = useState(type);
+    const [currentImage, setCurrentImage] = useState(imagen2);
+
+    const togglePasswordVisibility = () => {
+        if (inputType === 'password') {
+            setInputType('text');
+            setCurrentImage(imagen3);
+        } else {
+            setInputType('password');
+            setCurrentImage(imagen2);
+        }
+    };
+
     return (
         <div className={style.inputContainer}>
-            <img src={imagen1} className={style.icon} />
+            <img src={imagen1} className={style.icon} alt="input icon" />
             <input
                 className={style.input}
-                type={type}
+                type={inputType}
                 placeholder={placeholder}
                 value={value}
                 onChange={onChange}
             />
-            <img src={imagen2} className={style.icon2} />
+            {type === 'password' && (
+                <img
+                    src={currentImage}
+                    className={style.icon2}
+                    onClick={togglePasswordVisibility}
+                    alt="toggle visibility"
+                />
+            )}
         </div>
     );
 }
