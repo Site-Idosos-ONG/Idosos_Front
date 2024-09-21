c6d5c75edcc899c937718657ec934c832c3501ca
crvalho <danielcarvalhoo980@gmail.com>
2024-09-21 20:13:45 -0300

    feat: :hammer: Nav bar 1/2

--- src/pages/MenuPrincipalUsuario/MenuPrincipalUsuario.js
+++ src/pages/MenuPrincipalUsuario/MenuPrincipalUsuario.js
@@ -1,8 +1,8 @@
 import style from "./MenuPrincipalUsuario.module.css";
-import Foto from "../../assets/Img/FotoPerfil.png";
 import MenuInicial from "../../component/MenuInicial/MenuInicial";
-import ReactDOM from 'react-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGift, faCalendarDays, faCircleUser, faSquarePen, faChevronRight, faRightFromBracket} from '@fortawesome/free-solid-svg-icons'
+
 
 function MenuPrincipalUsuario() {
     const element = <FontAwesomeIcon icon="fa-solid fa-house" />
@@ -10,17 +10,45 @@ function MenuPrincipalUsuario() {
 
         <div className={style.fundo}>
             <nav className={style.sidebar}>
-                <div className={style.user}>
-                    <img src={Foto} className={style.userAvatar} alt="Avatar"></img>  
+                <div className={style.sidebarContent}>
+                    <ul className={style.sideItens}>
+                        
+                        <li className={style.sideItem + " " + style.active}>
+                        <FontAwesomeIcon icon={faCircleUser} style={{color: "#ffffff",}} />
+                            <span className={style.texto}>Perfil</span>
+                        </li>
+                        
+                        <li className={style.sideItem}>
+                            <FontAwesomeIcon icon={faGift} style={{color: "#ffffff",}} />
+                            <span className={style.texto}>Doações</span>
+                        </li>
+
+                        <li className={style.sideItem}>
+                            <FontAwesomeIcon icon={faCalendarDays} style={{color: "#ffffff",}} />
+                            <span className={style.texto}>Eventos</span>
+                        </li>
+
+                        <li className={style.sideItem}>
+                        <FontAwesomeIcon icon={faSquarePen} style={{color: "#fafafa",}} />
+                            <span className={style.texto}>Agendar Atividade</span>
+                        </li>
+                        
+                    </ul>
+                    
+                    <button className={style.btnRight}>
+                        <FontAwesomeIcon className={style.iconRight} icon={faChevronRight} style={{color: "#12125e",}} />
+                    </button>
                 </div>
 
-                <ul className={style.sideItens}>
-                    <li className={style.sideItem}>
-                        
-                    </li>
-                </ul>
-
+                <div className={style.logoutBtn}>
+                    <button className={style.btnLogout}>
+                        <FontAwesomeIcon icon={faRightFromBracket} style={{color: "#ffffff",}} />
+                        <span className={style.logoutTexto}>Sair</span>
+                    </button>
+                </div>
             </nav>
+
+            
             <MenuInicial />
     
            
